c5083ff60fe15d811cc8e61eedfc832ba068e269
yeona813 <aya813@naver.com>
2025-08-04 19:03:04 +0900

    [refactor] #SPOT-18 : Dropdown z-index 수정

--- src/shared/ui/Dropdown.tsx
+++ src/shared/ui/Dropdown.tsx
@@ -11,7 +11,7 @@ interface DropdownProps {
 export const Dropdown = ({ handleEdit, handleDelete, top, isDetail }: DropdownProps) => {
   return (
     <div
-      className={`absolute top-[${top}px] right-0 min-w-[163px] max-w-[200px] w-max h-[98px] rounded-[20px] bg-white shadow-box cursor-pointer`}>
+      className={`absolute top-[${top}px] right-0 min-w-[163px] max-w-[200px] w-max h-[98px] rounded-[20px] bg-white shadow-box cursor-pointer z-[1002]`}>
       <div className="px-5 py-[14px] flex gap-[26px] items-center" onClick={handleEdit}>
         <p className="text-sm font-medium text-gray-80">{isDetail ? "출발지" : "모임"} 수정하기</p>
         <img src={Edit} alt="edit" className="w-5 h-5" />
