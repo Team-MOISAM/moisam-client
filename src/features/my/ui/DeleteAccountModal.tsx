6f30d3867dc063ded7bc96b78b2a6acaa26abb9a
yeona813 <aya813@naver.com>
2025-08-12 15:18:28 +0900

    [fix] #SPOT-162 : 탈퇴하기 클릭 시 모달 추가

---
+++ src/features/my/ui/DeleteAccountModal.tsx
@@ -0,0 +1,29 @@
+import { Modal } from "@/shared/ui";
+
+interface DeleteAccountModalProps {
+  onClose: () => void;
+  onDelete: () => void;
+}
+
+export const DeleteAccountModal = ({ onClose, onDelete }: DeleteAccountModalProps) => {
+  return (
+    <Modal>
+      <div className="px-5 py-[28px] flex flex-col gap-1 items-center">
+        <span className="text-gray-90 text-md font-semibold">정말로 탈퇴하시겠어요?</span>
+        <p className="text-gray-40 text-sm font-medium">탈퇴 시 계정 정보가 모두 삭제돼요</p>
+      </div>
+      <div className="flex w-full">
+        <div
+          className="w-1/2 flex justify-center items-center py-3 text-gray-40 text-sm font-medium border-t border-t-gray-5 cursor-pointer"
+          onClick={onClose}>
+          더 써볼래요
+        </div>
+        <div
+          className="w-1/2 flex justify-center items-center py-3 text-white text-sm font-semibold border-t bg-gray-90 rounded-br-[20px] cursor-pointer"
+          onClick={onDelete}>
+          떠날래요
+        </div>
+      </div>
+    </Modal>
+  );
+};
--- src/features/my/ui/Menu.tsx
+++ src/features/my/ui/Menu.tsx
@@ -1,8 +1,12 @@
 import Arrow from "@/assets/icon/rightArrow.svg";
 import { useNavigate } from "react-router-dom";
 import { UseDeleteAccount } from "../hooks";
+import { useState } from "react";
+import { DeleteAccountModal } from "./DeleteAccountModal";
 
 export const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const navigate = useNavigate();
   const { mutate } = UseDeleteAccount();
 
@@ -25,24 +29,27 @@ export const Menu = () => {
       label: "탈퇴하기",
       hasArrow: false,
       onClick: () => {
-        mutate();
+        setIsOpen(true);
       },
     },
   ];
 
   return (
-    <div className="flex flex-col py-3 px-5">
-      {menuItems.map((item, idx) => (
-        <button
-          key={idx}
-          onClick={item.onClick}
-          className={`flex py-3 justify-between text-md font-medium text-left w-full ${
-            item.hasArrow ? "text-gray-60" : "text-gray-30"
-          }`}>
-          {item.label}
-          {item.hasArrow && <img src={Arrow} alt="arrow" className="w-4 h-4" />}
-        </button>
-      ))}
-    </div>
+    <>
+      <div className="flex flex-col py-3 px-5">
+        {menuItems.map((item, idx) => (
+          <button
+            key={idx}
+            onClick={item.onClick}
+            className={`flex py-3 justify-between text-md font-medium text-left w-full ${
+              item.hasArrow ? "text-gray-60" : "text-gray-30"
+            }`}>
+            {item.label}
+            {item.hasArrow && <img src={Arrow} alt="arrow" className="w-4 h-4" />}
+          </button>
+        ))}
+      </div>
+      {isOpen && <DeleteAccountModal onClose={() => setIsOpen(false)} onDelete={() => mutate()} />}
+    </>
   );
 };
