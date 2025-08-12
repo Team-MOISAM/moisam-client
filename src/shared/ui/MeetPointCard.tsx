e06bc7ea78e8b935ba391fd35440292c7b5a3d13
yeona813 <aya813@naver.com>
2025-08-12 16:52:09 +0900

    [fix] #SPOT-162 : MeetPointCard 핀 영역 너비 고정

--- src/shared/ui/MeetPointCard.tsx
+++ src/shared/ui/MeetPointCard.tsx
@@ -15,16 +15,19 @@ export const MeetPointCard = ({ placeName, onClick, isPlace = true }: MeetPointC
     <div
       className={`flex gap-3 p-3 rounded-2xl bg-white shadow-list w-full items-center ${onClick ? "cursor-pointer" : ""}`}
       onClick={onClick}>
-      <div className={`w-10 h-10 flex justify-center items-center rounded-xl ${isSelect ? "bg-sub-10" : "bg-gray-5"}`}>
+      <div
+        className={`w-10 h-10 flex flex-shrink-0 justify-center items-center rounded-xl ${isSelect ? "bg-sub-10" : "bg-gray-5"}`}>
         <img src={isSelect ? PinBlue : Pin} alt="pin" className="w-6 h-6" />
       </div>
-      <div className="w-full">
-        <p className={`text-xs ${isSelect ? "text-sub-sub" : "text-gray-40"}`}>
-          {isSelect ? "여기에서 모여요" : "어디서 만나실 건가요?"}
-        </p>
-        <span className="text-md font-semibold text-gray-90">{placeName ?? "장소를 정해보세요"}</span>
+      <div className="flex justify-between items-center w-full">
+        <div>
+          <p className={`text-xs ${isSelect ? "text-sub-sub" : "text-gray-40"}`}>
+            {isSelect ? "여기에서 모여요" : "어디서 만나실 건가요?"}
+          </p>
+          <span className="text-md font-semibold text-gray-90">{placeName ?? "장소를 정해보세요"}</span>
+        </div>
+        {isPlace && <img src={Arrow} alt="arrow" className="w-5 h-5" />}
       </div>
-      {isPlace && <img src={Arrow} alt="arrow" className="w-5 h-5" />}
     </div>
   );
 };
