68fd6528e710123300a10199e0010acf3f24743d
yeona813 <aya813@naver.com>
2025-08-18 18:08:33 +0900

    [fix] #SPOT-168 : 이미지 늦게 뜨는 문제 해결

--- src/shared/ui/PlaceCard.tsx
+++ src/shared/ui/PlaceCard.tsx
@@ -49,6 +49,7 @@ const PlaceCard = ({
             alt="placeImg"
             className="w-[68px] h-[68px] rounded-lg object-cover"
             onError={() => setHasImgError(true)}
+            loading="lazy"
           />
         )}
       </div>
