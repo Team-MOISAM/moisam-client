1f550587daba52e21750c6df149f01e45fc4c198
yeona813 <aya813@naver.com>
2025-08-18 18:07:19 +0900

    [fix] #SPOT-168 : 이미지 늦게 뜨는 문제 해결

--- src/features/detail/ui/Photo.tsx
+++ src/features/detail/ui/Photo.tsx
@@ -5,18 +5,18 @@ interface PhotoProps {
 export const Photo = ({ images }: PhotoProps) => {
   const length = images.length;
 
-  if (length < 1) return;
+  if (length < 1) return null;
   if (length > 5) images = images.slice(0, 5);
 
   if (length === 1) {
-    return <img src={images[0]} className="w-full h-[180px] object-cover" alt="image1" />;
+    return <img src={images[0]} className="w-full h-[180px] object-cover" alt="image1" loading="lazy" />;
   }
 
   if (length === 2) {
     return (
       <div className="flex w-full h-[180px]">
-        <img src={images[0]} className="w-1/2 border-r border-r-white object-cover" alt="image1" />
-        <img src={images[1]} className="w-1/2 object-cover" alt="image2" />
+        <img src={images[0]} className="w-1/2 border-r border-r-white object-cover" alt="image1" loading="lazy" />
+        <img src={images[1]} className="w-1/2 object-cover" alt="image2" loading="lazy" />
       </div>
     );
   }
@@ -24,10 +24,15 @@ export const Photo = ({ images }: PhotoProps) => {
   if (length === 3) {
     return (
       <div className="flex w-full h-[180px]">
-        <img src={images[0]} className="w-1/2 h-full object-cover border-r border-white" alt="image1" />
+        <img src={images[0]} className="w-1/2 h-full object-cover border-r border-white" alt="image1" loading="lazy" />
         <div className="flex flex-col w-1/2 h-full">
-          <img src={images[1]} className="w-full h-1/2 object-cover border-b border-white" alt="image2" />
-          <img src={images[2]} className="w-full h-1/2 object-cover" alt="image3" />
+          <img
+            src={images[1]}
+            className="w-full h-1/2 object-cover border-b border-white"
+            alt="image2"
+            loading="lazy"
+          />
+          <img src={images[2]} className="w-full h-1/2 object-cover" alt="image3" loading="lazy" />
         </div>
       </div>
     );
@@ -36,12 +41,22 @@ export const Photo = ({ images }: PhotoProps) => {
   if (length === 4) {
     return (
       <div className="flex w-full h-[180px]">
-        <img src={images[0]} className="w-1/2 h-full object-cover border-r border-white" alt="image1" />
+        <img src={images[0]} className="w-1/2 h-full object-cover border-r border-white" alt="image1" loading="lazy" />
         <div className="flex flex-col w-1/2 h-full">
-          <img src={images[1]} className="w-full h-1/2 object-cover border-b border-white" alt="image2" />
+          <img
+            src={images[1]}
+            className="w-full h-1/2 object-cover border-b border-white"
+            alt="image2"
+            loading="lazy"
+          />
           <div className="flex h-full">
-            <img src={images[2]} className="w-full h-1/2 object-cover border-r border-white" alt="image3" />
-            <img src={images[3]} className="w-full h-1/2 object-cover" alt="image4" />
+            <img
+              src={images[2]}
+              className="w-full h-1/2 object-cover border-r border-white"
+              alt="image3"
+              loading="lazy"
+            />
+            <img src={images[3]} className="w-full h-1/2 object-cover" alt="image4" loading="lazy" />
           </div>
         </div>
       </div>
@@ -51,15 +66,25 @@ export const Photo = ({ images }: PhotoProps) => {
   if (length === 5) {
     return (
       <div className="flex w-full h-[180px]">
-        <img src={images[0]} className="w-1/2 h-full object-cover border-r border-white" alt="image1" />
+        <img src={images[0]} className="w-1/2 h-full object-cover border-r border-white" alt="image1" loading="lazy" />
         <div className="flex flex-col w-1/2 h-ful">
           <div className="flex h-1/2 border-b-white border-b">
-            <img src={images[1]} className="w-full h-full object-cover border-r border-white" alt="image2" />
-            <img src={images[2]} className="w-full h-full object-cover" alt="image3" />
+            <img
+              src={images[1]}
+              className="w-full h-full object-cover border-r border-white"
+              alt="image2"
+              loading="lazy"
+            />
+            <img src={images[2]} className="w-full h-full object-cover" alt="image3" loading="lazy" />
           </div>
           <div className="flex h-1/2">
-            <img src={images[3]} className="w-full h-full object-cover border-r border-white" alt="image4" />
-            <img src={images[4]} className="w-full h-full object-cover" alt="image5" />
+            <img
+              src={images[3]}
+              className="w-full h-full object-cover border-r border-white"
+              alt="image4"
+              loading="lazy"
+            />
+            <img src={images[4]} className="w-full h-full object-cover" alt="image5" loading="lazy" />
           </div>
         </div>
       </div>
